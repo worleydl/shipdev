d9e33fe4ea18822810e02e306c4085b770bc693f
= <danworley@gmail.com>
2025-05-26 16:01:10 -0400

    UWP: Cleanslate port of soh with MT dll approach

--- soh/soh/OTRGlobals.cpp
+++ soh/soh/OTRGlobals.cpp
@@ -1154,7 +1154,7 @@ extern "C" void InitOTR(int argc, char* argv[]) {
     Ship::WiiU::Init(appShortName);
 #endif
 
-#ifdef _WIN32
+#if defined(_WIN32) && !defined(_UWP)
     char* tempVar = getenv("TEMP");
     std::filesystem::path tempPath;
     try {
--- soh/src/code/main.c
+++ soh/src/code/main.c
@@ -3,6 +3,11 @@
 #include <locale.h>
 #endif
 
+#ifdef _UWP
+#define SDLMAIN_DECLSPEC __declspec(dllexport)
+#include "SDL_main.h"
+#endif
+
 #include "global.h"
 #include "vt.h"
 #include "stdio.h"
